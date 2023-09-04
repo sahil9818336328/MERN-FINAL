9033e981f34b8b8b6c5fc7466a5c99d2532bab1b
Sahil khatri <Sahilkehsav1997@gmail.com>
2023-09-04 16:54:26 +0530

    password hashing, validation middleware, register and login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,21 @@
+import { StatusCodes } from 'http-status-codes'
+import User from '../models/userModal.js'
+import { comparePassword, hashPassword } from '../utils/passwordUtils.js'
+import { UnauthenticatedError } from '../errors/customErrors.js'
+
 export const register = async (req, res) => {
-  res.send('register')
+  const isFirstAccount = (await User.countDocuments()) === 0
+  req.body.role = isFirstAccount ? 'admin' : 'user'
+  const hashedPassword = await hashPassword(req.body.password)
+  req.body.password = hashedPassword
+  const user = await User.create(req.body)
+  res.status(StatusCodes.OK).json({ msg: 'User created successfully' })
 }
 
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email })
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password))
+  if (!isValidUser) throw new UnauthenticatedError('invalid credentials')
   res.send('login')
 }
--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -3,6 +3,7 @@ import { BadRequestError, NotFoundError } from '../errors/customErrors.js'
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js'
 import mongoose from 'mongoose'
 import Job from '../models/jobModels.js'
+import User from '../models/userModal.js'
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -42,4 +43,37 @@ export const validateIdParam = withValidationErrors([
     if (!job) throw new NotFoundError(`no job with id : ${value}`)
     // IF EVERYTHING'S GOOD, PASS THE CONTROL TO THE RESPECTIVE CONTROLLER
   }),
+])
+
+// REGISTER VALIDATION
+export const validateRegisterInput = withValidationErrors([
+  body('name').notEmpty().withMessage('name is required'),
+  body('email')
+    .notEmpty()
+    .withMessage('email is required')
+    .isEmail()
+    .withMessage('invalid email format')
+    .custom(async (email) => {
+      const user = await User.findOne({ email })
+      if (user) {
+        throw new BadRequestError('email already exists')
+      }
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 8 })
+    .withMessage('password must be at least 8 characters long'),
+  body('location').notEmpty().withMessage('location is required'),
+  body('lastName').notEmpty().withMessage('last name is required'),
+])
+
+//LOGIN VALIDATION
+export const validateLoginInput = withValidationErrors([
+  body('email')
+    .notEmpty()
+    .withMessage('email is required')
+    .isEmail()
+    .withMessage('invalid email format'),
+  body('password').notEmpty().withMessage('password is required'),
 ])
