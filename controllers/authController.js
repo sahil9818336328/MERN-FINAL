aa02dfee8ca05250663f84a0df6a2e41e051c8c7
Sahil khatri <Sahilkehsav1997@gmail.com>
2023-09-04 17:58:01 +0530

    Jwt implementation and authentication middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 import User from '../models/userModal.js'
 import { comparePassword, hashPassword } from '../utils/passwordUtils.js'
 import { UnauthenticatedError } from '../errors/customErrors.js'
+import { createJwt } from '../utils/tokenUtils.js'
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0
@@ -17,5 +18,12 @@ export const login = async (req, res) => {
   const isValidUser =
     user && (await comparePassword(req.body.password, user.password))
   if (!isValidUser) throw new UnauthenticatedError('invalid credentials')
-  res.send('login')
+  const token = createJwt({ userId: user._id, role: user.role })
+  const oneDay = 1000 * 60 * 60 * 24
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  })
+  res.status(StatusCodes.CREATED).json({ msg: 'User logged in' })
 }
--- server.js
+++ server.js
@@ -6,12 +6,15 @@ import jobRouter from './routes/jobRouter.js'
 import authRouter from './routes/authRouter.js'
 import mongoose from 'mongoose'
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js'
+import { authenticateUser } from './middleware/authMiddleware.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
 // TO HAVE ACCESS TO REQ.BODY
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 // ONLY RUN LOGS IN DEVELOPMENT
 if (process.env.NODE_ENV === 'development') {
@@ -19,7 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // ROUTERS
-app.use('/api/v1/jobs', jobRouter)
+app.use('/api/v1/jobs', authenticateUser, jobRouter)
 app.use('/api/v1/auth', authRouter)
 
 // FOR IN-COMING REQUESTS
