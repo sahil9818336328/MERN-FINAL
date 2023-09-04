29b26c5f4b70f5440761e48abc38d1f35a7b4d0b
Sahil khatri <Sahilkehsav1997@gmail.com>
2023-09-04 11:46:26 +0530

    created custom errors for clean and re-usibility of code

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,26 +1,26 @@
 import Job from '../models/jobModels.js'
+import { StatusCodes } from 'http-status-codes'
+import { NotFoundError } from '../errors/customErrors.js'
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({})
-  res.status(200).json({ jobs })
+  res.status(StatusCodes.OK).json({ jobs })
 }
 
 // CREATE JOB
 export const createJob = async (req, res) => {
   const job = await Job.create(req.body)
 
-  res.status(200).json({ job })
+  res.status(StatusCodes.CREATED).json({ job })
 }
 
 // GET SINGLE JOB
 export const getJob = async (req, res) => {
   const { id } = req.params
   const job = await Job.findById(id)
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` })
-  }
-  res.status(200).json({ job })
+  if (!job) throw new NotFoundError(`no job with id ${id}`)
+  res.status(StatusCodes.OK).json({ job })
 }
 
 // UPDATE JOB
@@ -34,7 +34,7 @@ export const updateJob = async (req, res) => {
     return res.status(404).json({ msg: `no job with id ${id}` })
   }
 
-  res.status(200).json({ msg: 'job modified', job: updatedJob })
+  res.status(StatusCodes.OK).json({ msg: 'job modified', job: updatedJob })
 }
 
 // DELETE JOB
@@ -45,5 +45,5 @@ export const deleteJob = async (req, res) => {
     return res.status(404).json({ msg: `no job with id ${id}` })
   }
 
-  res.status(200).json({ msg: 'job deleted', job: removedJob })
+  res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob })
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv'
 import morgan from 'morgan'
 import jobRouter from './routes/jobRouter.js'
 import mongoose from 'mongoose'
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js'
 
 dotenv.config()
 const app = express()
@@ -29,10 +30,7 @@ app.use('*', (req, res) => {
 })
 
 // WITHIN EXISTING REQUEST
-app.use((err, req, res, next) => {
-  console.log(err)
-  res.status(500).json({ msg: 'something went wrong' })
-})
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
